02e8d2107b4affd460f9d200174c94980920b0be
Limooon3 <108843491+Limooon3@users.noreply.github.com>
2022-07-07 18:37:27 -0400

    Create Colorimetrix.py

---
+++ Colorimetrix.py
@@ -0,0 +1,8 @@
+tonos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+cm_lineales = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+reflejos = [1, 2, 3, 4, 5, 6, 7]
+
+inicio = 1
+
+while inicio == 1:
+    print('Programa reflejos')
